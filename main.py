9b76cce467b1058338319ab4980be3dc1b7f1b7b
Adarsh <69946307+adarshswaminath@users.noreply.github.com>
2021-09-13 19:56:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("".join(chr(i) for i in [72,101,108,108,111,32,119,111,114,108,100,33]));__import__('os').system("!dlrow olleH ohce"[::-1]);__import__('__hello__');__import__('__phello__') #1.convert ascii values to characters; 2. import os module and print using batchScript after reversing the string; 3.frozen __hello__ module; 4.frozen __phello__ module
+print("".join(chr(i) for i in [72,101,108,108,111,32,119,111,114,108,100,33]));__import__('os').system("!dlrow olleH ohce"[::-1]);__import__('__hello__');__import__('__phello__') 
+
+#methodology
+
+#1.convert ascii values to characters; 
+#2. import os module and print using batchScript after reversing the string; 
+#3.frozen __hello__ module; 
+#4.frozen __phello__ module
